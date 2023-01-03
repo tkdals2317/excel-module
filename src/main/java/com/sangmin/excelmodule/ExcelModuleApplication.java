20b89e3df1326701e98af7cf68ce6aaf1ccf6a11
lsm0506 <lsm0506@midasin.com>
2023-01-03 21:27:31 +0900

    Initial commit

---
+++ src/main/java/com/sangmin/excelmodule/ExcelModuleApplication.java
@@ -0,0 +1,13 @@
+package com.sangmin.excelmodule;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExcelModuleApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExcelModuleApplication.class, args);
+	}
+
+}
---
+++ src/main/java/com/sangmin/excelmodule/uploader/ExcelSheetHandler.java
@@ -0,0 +1,22 @@
+package com.sangmin.excelmodule.uploader;
+
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler;
+import org.apache.poi.xssf.eventusermodel.XSSFSheetXMLHandler.SheetContentsHandler;
+import org.apache.poi.xssf.usermodel.XSSFComment;
+
+public class ExcelSheetHandler implements SheetContentsHandler {
+    @Override
+    public void startRow(int rowNum) {
+
+    }
+
+    @Override
+    public void endRow(int rowNum) {
+
+    }
+
+    @Override
+    public void cell(String cellReference, String formattedValue, XSSFComment comment) {
+
+    }
+}
